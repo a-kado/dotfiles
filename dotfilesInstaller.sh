fc20ef93a010bb0205ce95a80372e31d2887fc7f
ST14574 <st14574@LF02Q601701C98M.local>
2017-09-14 19:14:05 +0900

    update

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -1,5 +1,6 @@
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+# quit it because I wanna use prezto
+#ln -s ~/dotfiles/.zshrc ~/.zshrc
 git config --global core.excludesfile ~/.gitignore_global
 
