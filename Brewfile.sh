41269e9633ee762d9dfc2c7eeced7aa490cb4a4d
Atsushi Kadotani <kadotani@linecorp.com>
2018-11-20 15:13:45 +0900

    add postman

--- Brewfile.sh
+++ Brewfile.sh
@@ -45,6 +45,7 @@ brew cask install amazon-cloud-drive
 brew cask install java8
 brew cask install visual-studio-code
 brew cask install skitch
+brew cask install postman
 
 # Remove outdated versions
 brew cleanup
