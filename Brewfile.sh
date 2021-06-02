c94902c2f6bff34774efdcf63dabc86afe52b016
kadotani <kadotani@linecorp.com>
2021-06-02 21:13:51 +0900

    update packages.

--- Brewfile.sh
+++ Brewfile.sh
@@ -12,7 +12,7 @@ brew install zsh
 brew install git
 brew install vim
 brew install gcc
-brew install mysql@5.6
+brew install mysql
 brew install ag
 brew install libpng --universal
 brew install libjpeg --universal
@@ -22,10 +22,15 @@ brew install ctags
 brew install maven
 brew install node
 brew install npm
-brew install homebrew/versions/tomcat8
+brew install tomcat@8
 brew install jetty
 brew install redis
 brew install jq
+brew install python
+brew install gradle
+brew install nginx
+brew install openssl
+brew install docker
 
 # Packages for brew-cask
 brew install brew-cask
@@ -38,16 +43,18 @@ brew cask install alfred
 brew cask install iterm2
 brew cask install evernote
 brew cask install dropbox
-brew cask install karabiner
-brew cask install bee
 brew cask install skype
 brew cask install kindle
 brew cask install amazon-cloud-drive
 brew cask install java8
+brew cask install java11
 brew cask install visual-studio-code
 brew cask install skitch
 brew cask install postman
+brew cask install krisp
+brew cask install google-backup-and-sync
+brew cask install google-japanese-ime
+brew cask install deepl
 
 # Remove outdated versions
 brew cleanup
-
